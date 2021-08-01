69c565cb03c057ac37a4e57e69bc6707073ca6db
Shubham Ganguly <rickirod007@gmail.com>
2021-08-01 17:30:06 +0530

    added routes for subscriptions
    added /:username
    
    added /:username/:date

--- models/subscriptionModel.js
+++ models/subscriptionModel.js
@@ -3,17 +3,14 @@ const sequelize = require("./dbConnection");
 
 subscriptionAttributes = {
     id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    start_date: { type: DataTypes.DATEONLY, defaultValue: DataTypes.NOW, allowNull: false },
+    valid_till: { type: DataTypes.DATEONLY, allowNull: false}
 }
 
 class Subscription extends Model {}
 
 Subscription.init(subscriptionAttributes, 
-    { 
-        sequelize,
-        timestamps: true,
-        createdAt: 'start_date',
-        updatedAt: false
-    }
+    { sequelize }
 )
 
 module.exports = Subscription;
--- routes/subscriptionRoute.js
+++ routes/subscriptionRoute.js
@@ -1,4 +1,5 @@
 const { Subscription, Plan } = require("../models");
+const { Op } = require("sequelize");
 
 const router = require("express").Router();
 
@@ -10,27 +11,31 @@ router.get("/test", (req, res)=>{
 router.post("/", async (req,res)=>{
     let { user_name, plan_id, start_date } = req.body;
     let result = undefined;
+    let Validity = undefined;
     let Cost = 0;
     try{
         if(!((typeof user_name === "undefined") && (typeof plan_id === "undefined") && (typeof start_date === "undefined"))){
             try{
-                ({ Cost } = await Plan.findOne({where:{ id: plan_id }}));
+                ({ Cost, Validity } = await Plan.findOne({where:{ id: plan_id }}));
             }
             catch(err){
                 throw "No such Plan exists."
             }
+            let valid_date = new Date(new Date(start_date).getTime()+(Validity*24*60*60*1000));
             await Subscription.create({
                 user_name: user_name,
                 PlanId: plan_id,
-                start_date: start_date
+                start_date: start_date,
+                valid_till: valid_date
             })    
             result = {
                 status: "SUCCESS",
-                amount: +Cost
+                amount: Cost
             }
         }
     }
     catch(e){
+        res.status(400);
         result = {
             status: "FAILED",
             amount: -Cost
@@ -38,6 +43,72 @@ router.post("/", async (req,res)=>{
     }
     finally{
         res.status(200).send(result);
+    }
+})
+
+router.get("/:username", async(req, res)=>{
+    let { username } = req.params;
+    let result = undefined;
+    res.status(200);
+    res.setHeader("Content-Type","application/json");
+    try{
+        if(typeof username !== undefined){
+            result = await Subscription.findAll({
+                where: {
+                    user_name: username
+                },
+                attributes: ["PlanId", "start_date", "valid_till"]
+            })
+            if(result.length==0){
+                throw "No records found."
+            }
+        }
+    }
+    catch(err){
+        res.status(404);
+        res.setHeader("Content-Type", "plain/text");
+        result = err;
+    }
+    finally{
+        res.send(result);
+    }
+})
+
+router.get("/:username/:date", async (req, res)=>{
+    let { username, date } = req.params;
+    let result = undefined;
+    res.status(200);
+    try{
+        if((typeof username !== undefined) && (typeof date !== undefined)){
+            try{
+                result = await Subscription.findOne({where: {
+                    user_name: username,
+                    start_date: {
+                        [Op.like]: new Date(date)
+                    }
+                },
+                attributes: ["PlanId", "valid_till"]
+                })
+                result = {...result.dataValues};
+                result["days_left"] = (new Date(result["valid_till"]).getTime() - new Date().getTime())/(24*60*60*1000);
+                delete result["valid_till"];
+            }
+            catch(err){
+                res.status(404)
+                throw "No records founds";
+            }
+        }
+        else{
+            res.status(400)
+            throw "Parameters required are username and date"
+        }
+    }
+    catch(err){
+        res.setHeader("Content-Type", "plain/text")
+        result = err
+    }
+    finally{
+        res.send(result);
     }
 })
 
